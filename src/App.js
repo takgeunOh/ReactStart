ba548ade6df3a6f7ce7e8a9fd1d986e58e6daeb6
taKKen <99165619+takgeunOh@users.noreply.github.com>
2022-05-25 10:45:11 +0900

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,32 @@
+import "./styles.css";
+
+const SaramTitle = () => {
+  return (
+    <>
+      <h1>길동 반점 메뉴 목록</h1>
+      <h2>길동 반점 여름 특별식</h2>
+    </>
+  );
+};
+
+const Saram = () => {
+  return <h3>홍길동</h3>;
+};
+
+const Menu = (props) => {
+  return <h3>{props.data}</h3>;
+};
+
+const MenuItems = ["짜장면", "짬뽕", "팔보채", "마파두부", "탕수육"];
+
+export default function App() {
+  return (
+    <div className="App">
+      <SaramTitle />
+      <Saram />
+      {MenuItems.map((item) => {
+        return <Menu data={item} />;
+      })}
+    </div>
+  );
+}
